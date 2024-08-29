1b682b411f23eeb4b360fe1678353699fdb6f372
Mark Rybicki <mrybic87@gmail.com>
2024-08-29 12:55:58 -0500

    Second commit, ERD file and notes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sql_challenge
+
+First commit, uploaded the ERD txt file, uploaded schema and query files
+
+Second commit, uploaded the ERD screenshot, updated schema file with notes
--- pewlett_hackard_schema.sql
+++ pewlett_hackard_schema.sql
@@ -1,3 +1,10 @@
+--Created tables for titles, employees, dept_emp, departments, salaries and dept_managers for
+--the 6 csv files.  Employee number and department number are the two primary keys used for 
+--the tables, except for the titles table, which used the title_id as the primary.  The dept_emp
+--table has both the emp_no and dept_no as primary keys, since neither column contains unique 
+--values, as each department can have multiple employees, and each employee can work for 
+--multiple departments.
+
 create table titles (
 title_id varchar not null primary key,
 title varchar not null
